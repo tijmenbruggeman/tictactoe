e1cf0fa698d971027d46acdb5fc91a3c9a73534a
tijmen <tijmenbruggeman@gmail.com>
2023-03-23 13:29:35 +0100

    Write first test

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 2. "test": "vitest", to package.json
 3. Create first testsuite for game
 4. Add types for vitest to tsconfig
+5. Create first test for game
--- src/composables/game.test.ts
+++ src/composables/game.test.ts
@@ -1,3 +1,9 @@
-describe('game', () => {
+import { describe, test } from 'vitest';
+import { onClick, startGame } from "./game";
 
+describe('game', () => {
+    test('when I click the button and the game hasn`t started, startGame() is called', () => {
+        onClick();
+        expect(startGame).toBeCalled()
+    })
 });
